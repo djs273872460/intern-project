e8b1e1b07421f1e1a463ab5e128f5a374d5f8fd3
大奔的大葱 <273872460@qq.com>
2020-12-07 18:17:30 +0800

    v1.1

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ module.exports = {
   devServer: {
     proxy: {
       '/dev-api': {
-        target: 'http://140.143.247.32/api',
         changeOrigin: true,
         pathRewrite: {    
           '^/dev-api': ''
